61fc9f6e06200063bf03ca7bf63c7d866fd0c2e4
Pheejung <gmlwjd2116@naver.com>
2019-04-10 11:28:17 +0900

    인트로 수정 테스트

--- app/src/main/java/com/example/userinterface1/IntroActivity.java
+++ app/src/main/java/com/example/userinterface1/IntroActivity.java
@@ -1,13 +1,36 @@
 package com.example.userinterface1;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class IntroActivity extends AppCompatActivity {
+    private Handler handler;
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        init();
+
+        handler.postDelayed(runnable, 3000);
+
+
     }
+
+    public void init() {
+        handler = new Handler();
+    }
+
+
 }
